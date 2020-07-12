924cf896fb344ac7d525d1bf4edc7e815749cd2a
mayurchourasiya <30589146+mayurchourasiya@users.noreply.github.com>
2020-07-12 13:17:08 +0530

    Changes to show image

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -14,4 +14,6 @@ export class AuthorsComponent {
     this.authors = service.getAuthors();
    }
 
+   imgSource = "https://dl1.cbsistatic.com/i/2017/06/14/7b9c8f4d-838c-43da-9e2d-4053ad1e1624/a73f830efd30ae8dcb10b5cc17ef3558/imgingest-8648420337569711989.png";
+
 }
